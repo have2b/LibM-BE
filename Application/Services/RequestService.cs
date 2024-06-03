ec90a083c634f28dddbb8dc9a532eaa86e624e33
tlongdepzaivaicalon <thanhlongvu156@gmail.com>
2024-06-03 19:17:07 +0700

    feat: add request service

---
+++ Application/Services/RequestService.cs
@@ -0,0 +1,74 @@
+using Application.Contracts;
+using Application.DTOs;
+using Application.Utils;
+using Core.Contracts;
+using Core.Entities;
+using Mapster;
+
+namespace Application.Services
+{
+    public class RequestService : IRequestService
+    {
+        private readonly IRepositoryManager _repository;
+
+        public RequestService(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+        public async Task<RequestDto> CreateRequestAsync(RequestDto model)
+        {
+            var requestEntity = model.Adapt<Request>();
+
+            _repository.Request.CreateRequest(requestEntity);
+            await _repository.SaveAsync();
+
+            return requestEntity.Adapt<RequestDto>();
+        }
+
+        public async Task DeleteRequestAsync(Guid requestId)
+        {
+            var request = await EntityHelper
+                            .GetEntityAndCheckIfItExists
+                            (
+                                requestId,
+                                _repository.Request.GetRequestAsync
+                            );
+            _repository.Request.DeleteRequest(request);
+            await _repository.SaveAsync();
+        }
+
+        public async Task<IEnumerable<RequestDto>> GetRequestsAsync()
+        {
+            var requests = await _repository.Request.GetAllRequestsAsync();
+
+            var requestsDto = requests.Adapt<IEnumerable<RequestDto>>();
+
+            return requestsDto;
+        }
+
+        public async Task<RequestDto> GetRequestByIdAsync(Guid id)
+        {
+            var request = await EntityHelper
+                            .GetEntityAndCheckIfItExists
+                            (
+                                id,
+                                _repository.Request.GetRequestAsync
+                            );
+
+            return request.Adapt<RequestDto>();
+        }
+
+        public async Task UpdateRequestAsync(Guid requestId, RequestDto model)
+        {
+            var request = await EntityHelper
+                            .GetEntityAndCheckIfItExists
+                            (
+                                requestId,
+                                _repository.Request.GetRequestAsync
+                            );
+
+            model.Adapt(request);
+            await _repository.SaveAsync();
+        }
+    }
+}
--- Core/Contracts/IRequestRepository.cs
+++ Core/Contracts/IRequestRepository.cs
@@ -4,9 +4,9 @@ namespace Core.Contracts
 {
     public interface IRequestRepository
     {
-        Task<IEnumerable<Request>> GetAllBookRequestsAsync();
-        Task<Request> GetBookRequestAsync(Guid requestId);
-        void CreateBookRequest(Request request);
-        void DeleteBookRequest(Request request);
+        Task<IEnumerable<Request>> GetAllRequestsAsync();
+        Task<Request> GetRequestAsync(Guid requestId);
+        void CreateRequest(Request request);
+        void DeleteRequest(Request request);
     }
 }
