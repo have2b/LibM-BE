d1eb0df4b2a5ee0a89b0f753834d9246a756d2ef
tlongdepzaivaicalon <thanhlongvu156@gmail.com>
2024-06-04 12:27:47 +0700

    feat: add repositories for RequestDetail

--- Core/Contracts/IRequestDetailRepository.cs
+++ Core/Contracts/IRequestDetailRepository.cs
@@ -1,7 +1,11 @@
+using Core.Entities;
+
 namespace Core.Contracts
 {
     public interface IRequestDetailRepository
     {
-
+        Task<List<Book>> GetBooksByRequest(Guid requestId);
+        Task<List<Request>> GetRequestsByBook(Guid bookId);
+        void Add(Guid requestId, List<Guid> bookIds);
     }
 }
--- Infrastructure/Repositories/RequestDetailRepository.cs
+++ Infrastructure/Repositories/RequestDetailRepository.cs
@@ -1,11 +1,32 @@
 using Core.Contracts;
 using Core.Entities;
 using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories
 {
     public class RequestDetailRepository(ApplicationDbContext context) : RepositoryBase<RequestDetail>(context), IRequestDetailRepository
     {
+        public void Add(Guid requestId, List<Guid> bookIds)
+        {
+            foreach (var bookId in bookIds)
+            {
+                Create(new RequestDetail { RequestId = requestId, BookId = bookId });
+            }
+        }
 
+        public async Task<List<Book>> GetBooksByRequest(Guid requestId)
+        {
+            var requestDetails = GetAll();
+            return await requestDetails
+                        .Where(rd => rd.RequestId == requestId)
+                        .Select(rd => rd.Book)
+                        .ToListAsync();
+        }
+
+        public Task<List<Request>> GetRequestsByBook(Guid bookId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
